3b8a9b2c4afcc3e76e54ce8e68a6d5eeb05c4b42
Roxana149 <r.soto@uniandes.edu.co>
2026-05-03 20:23:18 -0500

    view

--- App/view.py
+++ App/view.py
@@ -1,6 +1,6 @@
 import sys
 import App.logic as logic
-# TODO Realice la importación de lista como estructura de datos auxiliar para la presentación de los resultados
+from DataStructures.List import array_list as al
 
 """
 La vista se encarga de la interacción con el usuario.
